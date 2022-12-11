c18e92cb50c04d10f0b9c3a24422edfd2bdb0b93
Kenji.N <1314webdev@gmail.com>
2022-12-11 19:04:58 -0300

    Setup Jest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import HeroHeader from "../components/HeroHeader";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
           media="(prefers-color-scheme: dark)"
         />
       </Head>
-      <HeroHeader />
+      <Header isHero={true} />
     </div>
   );
 }
