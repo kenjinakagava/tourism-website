9a26d5796f0d61a42d59c0fbf2168d4149f86615
Kenji.N <1314webdev@gmail.com>
2022-12-11 14:26:58 -0300

    Fix mobile Hero Banner

--- components/HeroHeader.tsx
+++ components/HeroHeader.tsx
@@ -1,19 +1,17 @@
 import Image from "next/image";
 import styled from "styled-components";
 
-import RioDeJaneiro from "../public/Riodejaneiro.jpg";
+import RioDeJaneiroFullHD from "../public/Riodejaneirofullhd.webp";
+import RioDeJaneiroHD from "../public/Riodejaneirohd.webp";
+import RioDeJaneiroMobile from "../public/Riodejaneiromobile.webp";
+
 import Header from "./Header";
-
-const HeroBanner = styled(Image)`
-  object-fit: cover;
-`;
-
 const HeroTagline = styled.h1`
   //
   display: flex;
   flex-direction: column;
   justify-content: center;
-  min-height: 100vh;
+  min-height: calc(100vh - 76px);
   max-width: 90%;
   margin: 0 auto;
   color: white;
@@ -26,8 +24,9 @@ const HeroTagline = styled.h1`
   }
 `;
 
-const HeroWrapper = styled.div`
+const HeroBanner = styled.img`
   position: absolute;
+  object-fit: cover;
   top: 0;
   left: 0;
   width: 100%;
@@ -45,12 +44,14 @@ const GradientText = styled.span`
 const HeroHeader = () => {
   return (
     <Header>
-      <HeroWrapper>
-        <HeroBanner src={RioDeJaneiro} alt="" fill={true} />
-        <HeroTagline>
-          Brazil, a <GradientText>paradise</GradientText>
-        </HeroTagline>
-      </HeroWrapper>
+      <HeroBanner
+        src={RioDeJaneiroFullHD.src}
+        alt=""
+        srcSet={`${RioDeJaneiroMobile} 880w, ${RioDeJaneiroHD} 720w, ${RioDeJaneiroFullHD} 1080w`}
+      />
+      <HeroTagline>
+        Brazil, a <GradientText>paradise</GradientText>
+      </HeroTagline>
     </Header>
   );
 };
